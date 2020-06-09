e5c604c9bff262ca2b4cfe49c8c0554013d362d1
daneccf <danielecavalcante@alu.ufc.br>
2020-06-08 22:44:49 -0300

    entities

---
+++ TrexRunner/Entities/Trex.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+//using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrexRunner.Graphics;
+
+namespace TrexRunner.Entities {
+    public class Trex : IGameEntity {
+
+        private const int TREX_DEFAULT_SPRITE_POS_X = 848;
+        private const int TREX_DEFAULT_SPRITE_POS_Y = 0;
+        private const int TREX_DEFAULT_SPRITE_WIDTH = 44;
+        private const int TREX_DEFAULT_SPRITE_HEIGHT = 52; 
+
+        public Sprite Sprite { get; private set; }
+
+        public TrexState State { get; private set; }
+
+        public Vector2 Position { get; set; }
+
+        public bool isAlive { get; private set; }
+
+        public float Speed { get; private set; }
+
+        public int DrawOrder { get; set; }
+
+        public Trex(Texture2D spriteSheet, Vector2 position) {
+
+            Sprite = new Sprite(spriteSheet, TREX_DEFAULT_SPRITE_POS_X, TREX_DEFAULT_SPRITE_POS_Y, TREX_DEFAULT_SPRITE_WIDTH, TREX_DEFAULT_SPRITE_HEIGHT);
+            Position = position;
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime) {
+            Sprite.Draw(spriteBatch, this.Position);
+        }
+
+        public void Update(GameTime gameTime) {
+        }
+    }
+}
